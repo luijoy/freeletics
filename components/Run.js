1f64eb7f4f2a45d4d85da4899113500dabae09ea
Lui Kohl <lui.kohl@gmail.com>
2018-10-30 13:13:54 +0100

    added on position change

--- components/Run.js
+++ components/Run.js
@@ -2,13 +2,29 @@ import * as React from 'react';
 import {StyleSheet, View} from 'react-native';
 import {MapView, Location} from 'expo';
 
+type Position = {
+  timestamp: number,
+  coords: {
+    latitude: number,
+    longitude: number,
+    accuracy: number,
+  },
+};
+
 type RunProps = {
   distance: number,
   latitude: number,
   longitude: number,
 };
 
-export default class Run extends React.Component<RunProps> {
+type RunState = {
+  positions: Position[],
+};
+
+export default class Run extends React.Component<RunProps, RunState> {
+  state = {
+    positions: [],
+  };
   async componentDidMount () {
     const options = {
       enableHighAccuracy: true,
@@ -24,10 +40,13 @@ export default class Run extends React.Component<RunProps> {
   componentWillUnmount () {
     this.listener.remove ();
   }
-  onPositionChange = position => {
+  onPositionChange = (position: Position) => {
     console.log ({position});
+    this.setState ({positions: [...this.state.positions, position]});
   };
   render (): React.Node {
+    const {positions} = this.state;
+    const currentPosition = positions;
     return (
       <View style={styles.container}>
         <MapView provider="google" style={styles.map} />
