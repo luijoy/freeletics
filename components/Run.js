87ed3640e4794291e8f39aaf02c331760db81f75
Lui Kohl <lui.kohl@gmail.com>
2018-10-30 13:04:01 +0100

    console log is working

--- components/Run.js
+++ components/Run.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import {StyleSheet, View} from 'react-native';
-import {MapView} from 'expo';
+import {MapView, Location} from 'expo';
 
 type RunProps = {
   distance: number,
@@ -9,6 +9,24 @@ type RunProps = {
 };
 
 export default class Run extends React.Component<RunProps> {
+  async componentDidMount () {
+    const options = {
+      enableHighAccuracy: true,
+      timeInterval: 1000,
+      distanceInterval: 1,
+    };
+    this.listener = await Location.watchPositionAsync (
+      options,
+      this.onPositionChange
+    );
+  }
+
+  componentWillUnmount () {
+    this.listener.remove ();
+  }
+  onPositionChange = position => {
+    console.log ({position});
+  };
   render (): React.Node {
     return (
       <View style={styles.container}>
